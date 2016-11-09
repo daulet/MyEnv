d493b7deb3de0e173193b25fd92b07959d930558
Daulet Zhanguzin <dazhangu@microsoft.com>
2016-11-09 12:28:06 -0800

    Add command line environment loading into powershell environment

---
+++ profile.cmd.ps1
@@ -0,0 +1,52 @@
+# Copy command line environment variables to powershell environment
+
+$EnvFilePath = $Env:CmdEnvFilePath
+if ($EnvFilePath -eq $null) {
+  # quit if no environment file is provided 
+  return
+}
+
+# merge two paths
+function Merge-Path($OldPath, $IncPath)
+{
+  $MergedPath = ($IncPath -split ";") + ($OldPath -split ';') `
+  |? {$_.Trim().Length -gt 0} `
+  | Select-Object -Unique
+
+  $NewPath = $MergedPath -join ";"
+
+  Write-Verbose "New Path:"
+  Write-Verbose $NewPath
+
+  return $NewPath
+}
+
+$TempFilePath = [System.IO.Path]::GetTempFileName()
+
+Write-Host "Init environment with $($EnvFilePath)" -Foreground Blue
+cmd /c "$EnvFilePath && set > $TempFilePath"
+
+if (-not (Test-Path $TempFilePath)) {
+  Write-Error "Cannot find expected temp file $TempFilePath" -Category InvalidData
+  return
+}
+
+$lines = cat $TempFilePath
+Remove-Item -Force $TempFilePath
+
+Write-Host "Importing $($lines.Count) environment variables" -Foreground Blue
+foreach ($line in $lines) {
+  $name, $value = $line -split '=', 2
+
+  #Write-Host "Overwrite [$name] with $value"
+  if ($name -eq "Path") {
+    $OldPath = $env:Path
+    $IncPath = $value
+    $mergedPath = Merge-Path $OldPath $IncPath
+
+    Set-Item -Path "Env:Path" -Value $mergedPath
+  } else {
+    
+    Set-Item -Path "Env:$name" -Value $value
+  }
+}
--- profile.ps1
+++ profile.ps1
@@ -3,6 +3,9 @@ Push-Location $PsScriptRoot
 # Load aliases
 .\profile.alias.ps1
 
+# Load command line environment
+.\profile.cmd.ps1
+
 # Load posh-git profile
 .\profile.posh-git.ps1
 
